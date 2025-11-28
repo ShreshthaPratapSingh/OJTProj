d108ea27f3b100dda328e7347d6a4ccdf7301b95
ShreshthaPratapSingh <shreshthapratap0@gmail.com>
2025-11-28 12:35:53 +0530

    Signup working

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bg from "../assets/LandingBG.png";
 import Logo from "../assets/logo.png"
 import { BriefcaseBusiness } from 'lucide-react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-function LoginPage() {
+function SignupPage() {
+
+    const navigate = useNavigate()
+
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [error, setError] = useState("")
+
+    async function handleSignUp() {
+        setError("")
+
+        const response = await fetch("http://127.0.0.1:8000/api/authentication/register/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            })
+        })
+        if (response.ok) {
+            console.log("login successful")
+            navigate('/dashboard')
+        }
+        else {
+            const data = await response.json();
+            setError(JSON.stringify(data));
+        }
+    }
     return (
         <div className="relative w-full h-screen bg-cover bg-no-repeat overflow-y-hidden" style={{ backgroundImage: `url(${bg})` }}>
             <div className="absolute inset-0 bg-black/70"></div>
@@ -15,12 +47,13 @@ function LoginPage() {
                         Get your ledger now! <BriefcaseBusiness size={30} />
                     </div>
                     <div className="input flex flex-col gap-4 mt-10 items-center">
-                        <input type="text" placeholder='Enter username' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' />
-                        <input type="email" placeholder='Enter email' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' />
-                        <input type="password" placeholder='Enter password' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' />
+                        <input type="text" placeholder='Enter username' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input type="email" placeholder='Enter email' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" placeholder='Enter password' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
+                    {error && <p className="text-red-400 text-center text-base">{error}</p>}
                     <div className="buttons flex flex-col items-center text-xl gap-2">
-                        <NavLink to="/dashboard" className='bg-blue-500 rounded-full w-1/3 border-2 border-blue-300 py-1 hover:cursor-pointer hover:shadow-[0px_0px_5px_white] transition text-center'><button>Signup</button></NavLink>
+                        <button onClick={handleSignUp} className='bg-blue-500 rounded-full w-1/3 border-2 border-blue-300 py-1 hover:cursor-pointer hover:shadow-[0px_0px_5px_white] transition text-center'>Signup</button>
                         <NavLink to='/loginPage'>
                             <span className='text-white text-xs font-light hover:cursor-pointer'>Already have an account? Login here</span>
                         </NavLink>
@@ -31,4 +64,4 @@ function LoginPage() {
     )
 }
 
-export default LoginPage
+export default SignupPage
