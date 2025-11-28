c4d73fba2b5cc81df9ad92039bceac6240efc0e1
ShreshthaPratapSingh <shreshthapratap0@gmail.com>
2025-11-28 13:12:09 +0530

    Login working

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
+import { useState } from 'react';
 import bg from "../assets/LandingBG.png";
 import Logo from "../assets/logo.png"
 import { BriefcaseBusiness } from 'lucide-react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+
+    const navigate = useNavigate()
+
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [error, setError] = useState("")
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        const data = {
+            username: username,
+            password: password
+        }
+
+        const response = await fetch("http://127.0.0.1:8000/api/token/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        })
+        if (response.ok) {
+            console.log("login successful")
+            navigate('/dashboard')
+        }
+        else {
+            const data = await response.json();
+            setError(JSON.stringify(data));
+        }
+
+        const result = await response.json()
+
+        localStorage.setItem("access", result.access)
+        localStorage.setItem("refresh", result.refresh)
+
+        console.log("Logged in!", result);
+    }
+
+
     return (
         <div className="relative w-full h-screen bg-cover bg-no-repeat overflow-y-hidden" style={{ backgroundImage: `url(${bg})` }}>
             <div className="absolute inset-0 bg-black/70"></div>
@@ -15,11 +56,12 @@ function LoginPage() {
                         Secure Access to Your Ledger <BriefcaseBusiness size={30} />
                     </div>
                     <div className="input flex flex-col gap-4 mt-10 items-center">
-                        <input type="text" placeholder='Enter username' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' />
-                        <input type="password" placeholder='Enter password' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' />
+                        <input type="text" placeholder='Enter username' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input type="password" placeholder='Enter password' className='text-white text-lg border border-gray-500 rounded-full px-4 py-1 placeholder:font-light font-light w-3/4' value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
+                    {error && <p className="text-red-400 text-center text-base">{error}</p>}
                     <div className="buttons flex flex-col items-center text-xl gap-2">
-                        <NavLink to="/dashboard" className='bg-blue-500 rounded-full w-1/3 border-2 border-blue-300 py-1 hover:cursor-pointer hover:shadow-[0px_0px_5px_white] transition text-center'><button>Login</button></NavLink>
+                        <button className='bg-blue-500 rounded-full w-1/3 border-2 border-blue-300 py-1 hover:cursor-pointer hover:shadow-[0px_0px_5px_white] transition text-center' onClick={handleLogin}>Login</button>
                         <NavLink to='/signupPage'>
                             <span className='text-white text-xs font-light hover:cursor-pointer'>Don't have an account? Signup here</span>
                         </NavLink>
